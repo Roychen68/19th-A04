55712d55af52cb82f73ba5a172bca1812f433c9c
roychen <aeustusr3.57@gmail.com>
2026-08-15 13:20:19 +0800

    adding vue

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
-$(function () {
-    function hash() {
-        console.log(location.hash);
-    }
+import { createApp, ref } from "vue";
 
-    $(window).on("hashchange",hash())
-})
+createApp({
+    data() {
+        return {
+            navigation: {
+                index: "首頁",
+                admin: "系統管理",
+                journal: "旅人日記",
+                forecast: "極光預報"
+            }
+        }
+    },
+    methods: {
+        hash() {
+            let breadcrumbs = new Set(["首頁"])
+            let current = location.hash.substring(1)
+            breadcrumbs.add(navigation[current])
+        }
+    },
+    mounted() {
+        $(window).on("hashchange", this.hash)
+    }
+}).mount("#app")
+
+// $(function () {
+//     const navigation = {
+//         index: "首頁",
+//         admin: "系統管理",
+//         journal: "旅人日記",
+//         forecast: "極光預報"
+//     };
+
+//     function hash() {
+//         let breadcrumbs = new Set(["首頁"])
+//         let current = location.hash.substring(1)
+//         breadcrumbs.add(navigation[current])
+//     }
+
+//     $(window).on("hashchange", hash)
+// })
