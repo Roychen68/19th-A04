07e95b0274f167435d8414d06774ac073bb1ba7f
roychen <aeustusr3.57@gmail.com>
2026-08-19 03:12:41 +0800

    forecast setting

--- api/get.php
+++ api/get.php
@@ -5,11 +5,11 @@ switch ($action) {
     case 'aurora':
         header("Content-Type: application/json");
         $result = file_get_contents("https://space.fmi.fi/MIRACLE/RWC/r-index/api/map-en-esa.json");
-        echo json_encode($result);
+        echo $result;
         break;
     
     default:
         # code...
         break;
-}
+} 
 ?>
